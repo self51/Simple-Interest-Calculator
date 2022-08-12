b565f97c71b6fffe7334ad1ff6f00992f44d8add
se1f4 <58149320+se1f4@users.noreply.github.com>
2022-08-12 23:00:24 +0300

    completed

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# coding-project-template
+# **Simple-Interest-Calculator**
+
+#### About
+This is a pet-project, it should not be used for commercial purposes!
+<br/>Simple Interest Calculator, will compute the interest
+<br/>one can earn given the amount you wish to save, 
+<br/>the number of years you can keep it invested
+<br/>and the interest rate the bank would pay you.
+
+Made by `Self`.
--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = principal * (rate / 100) * years;
+
+    if (principal > 1) {
+        document.getElementById('principal_val').innerText = "If you deposit " + principal;
+        document.getElementById('rate_val').innerText = "at an interest rate of " + rate;
+        document.getElementById('interest_val').innerText = "You will receive an amount of " + interest;
+        document.getElementById('years_val').innerText = "in the year " + year;
+
+    } else {
+        alert("Enter a positive number!");
+    }
 }
-        
+
+function change()
+{
+    var result = document.getElementById("result");
+    var rate = document.getElementById("rate");
+    result.innerText = rate.value;
+}
+
